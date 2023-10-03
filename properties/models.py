acd6713d2bfc993a64a524421dbfaec221b74d2f
OmoshLawi <lawiomosh3@gmail.com>
2023-10-03 12:39:02 +0300

    Improoved property model though still not complete...also improove home screen from various home templates

--- properties/models.py
+++ properties/models.py
@@ -5,24 +5,25 @@ from django.db import models
 class PropertyType(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
+    icon_name = models.CharField(max_length=50)
 
 
 class PropertyStatus(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=20)
     description = models.TextField(null=True, blank=True)
-    status_code = models.CharField(max_length=255, unique=True)
+    status_code = models.CharField(max_length=20, unique=True)
 
 
 class Property(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=50)
     type = models.ForeignKey("properties.PropertyType", on_delete=models.CASCADE, related_name='properties')
     status = models.ForeignKey("properties.PropertyStatus", on_delete=models.CASCADE, related_name='properties')
     price = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
-
-    address = models.ImageField(null=False, blank=False)
-    city = models.ImageField(null=False, blank=False)
-    state = models.ImageField(null=False, blank=False)
-    zipcode = models.ImageField(null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=50)
 
 
 class PropertyImage(models.Model):
@@ -32,7 +33,7 @@ class PropertyImage(models.Model):
 
 class PropertyUnit(models.Model):
     property = models.ForeignKey("properties.Property", on_delete=models.CASCADE, related_name='units')
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=50)
     status = models.ForeignKey("properties.PropertyStatus", on_delete=models.CASCADE, related_name='units')
     price = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
 
@@ -44,4 +45,5 @@ class PropertyUnitImage(models.Model):
 
 class PropertyUnitAmenity(models.Model):
     unit = models.ForeignKey("properties.PropertyUnit", on_delete=models.CASCADE, related_name='amenities')
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
