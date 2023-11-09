5b1a3e0ae7be7a29953cf3fe7b71477abce2e3a1
Laurent Ouma <lawiomosh3@gmail.com>
2023-11-09 22:07:00 +0300

    Split properties listing int highly reusable components and works perfectly

--- stakeholders/admin.py
+++ stakeholders/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from stakeholders.models import PropertyOwner, PropertyStaff, Tenant, PropertyStaffRole
+from stakeholders.models import PropertyOwner, PropertyStaff, Tenant, PropertyStaffRole, Person
 
 
 # Register your models here.
@@ -13,14 +13,20 @@ class PropertyOwnerAdmin(admin.ModelAdmin):
 
 @admin.register(PropertyStaff)
 class PropertyStaffAdmin(admin.ModelAdmin):
-    list_display = ('person',  'property', 'created_at', 'updated_at')
+    list_display = ('person', 'property', 'created_at', 'updated_at')
 
 
 @admin.register(Tenant)
 class TenantAdmin(admin.ModelAdmin):
-    list_display = ('person',  'unit', 'created_at', 'updated_at')
+    list_display = ('person', 'unit', 'created_at', 'updated_at')
 
 
 @admin.register(PropertyStaffRole)
 class PropertyStaffRoleAdmin(admin.ModelAdmin):
-    list_display = ('staff',  'role', 'created_at', 'updated_at')
+    list_display = ('staff', 'role', 'created_at', 'updated_at')
+
+
+@admin.register(Person)
+class PropertyStaffRoleAdmin(admin.ModelAdmin):
+    list_display = (
+        'user', 'surname', 'first_name', 'last_name', 'phone_number', 'date_of_birth', 'gender',)
